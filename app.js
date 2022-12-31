e2db840c3067213d5e98cd4e854a649e2ef6ad80
Pedro <pm.catarino@campus.fct.unl.pt>
2022-12-31 13:49:06 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,150 @@
+const content1 = "<figure id = 'fig1'><img src='/images/CarlosRibeiro.png' alt='Carlos Ribeiro'width='194' height='287'>\
+<figcaption> Retrato de Carlos Ribeiro</figcaption></figure>\
+<h3>Torvossauros Gurneyi</h3>\
+<h5>Classificação:&nbspTerópode<br>Período:&nbspJurássico Superior</h5>\
+<p>O primeiro registo fóssil de Portugal foi feito pelo geólogo Carlos Ribeiro, em 1863, que descobriu um dente de um dinossauro carnívoro, que posteriormente foi denominado Torvosaurus Gurneyi. Esta descoberta foi feita em Porto das Barcas, Lourinhã.<p>"
+
+const content2 = "<figure id = 'fig2-1'><img src='/images/allosaurus.jpg' alt='Allosaurus'width='288' height='189'>\
+<figcaption>Fóssil de Allosaurus europaeus</figcaption></figure>\
+<figure id = 'fig2-2'><img src='/images/allosaurusPata.jpg' alt='AllosaurusPata 'width='288' height='189'>\
+<figcaption>Pata de Allosaurus encontrada em Cambelas</figcaption></figure>\
+<h3>Allossaurus europaeus (fragilis)</h3> <h4><h5>Classificação:&nbspTerópode<br>Período:&nbspJurássico Superior</h5>\
+<p>Este era um dinossauro bípede de grandes dimensões. O Allosaurus europaeus tem tantas semelhanças com o Allosaurus fragilis que, para alguns, é considerada a mesma espécie. A descoberta de fósseis Allosaurus teve grande importância pois, até à data, apenas eram conhecidos fósseis Allossaurus nos Estados Unidos da América, logo, a mais recente descoberta, aponta para a migração destes dinossauros da América do Norte para a Europa, indicando a existência de uma “ponte” terrestre entre os dois continentes atuais. Parte do esqueleto fossilizado deste dinossauro, foi encontrado na Praia de Vale Frade, perto da Lourinhã, num estado muito bem conservado. Foi descrito em 2006 por Octávio Mateus.<p>"
+
+const content3 = "<figure id = 'fig3-1'><img src='/images/lourinhanhosaurus.jpg' alt='Lourinhanhosaurus'width='337' height='225'>\
+<figcaption>Réplica de Lourinhanhosaurus antunesi,<br>presente no Museu da Lourinhã</figcaption></figure>\
+<figure id = 'fig3-2'><img src='/images/embriao.png' alt='LourinhanhosaurusEmbriao'width='328' height='225'>\
+<figcaption>Embrião de Lourinhanhosaurus antunesi</figcaption></figure>\
+<h3>Lourinhanhosaurus antunesi</h3> <h5>Classificação:&nbspTerópode<br>Período:&nbspJurássico Superior</h5>\
+<p>Os primeiros registos fósseis desta espécie foram descobertos em 1982, em Peralta. Em Porto das Barcas foi também encontrado um fémur deste dinossauro carnívoro de dimensões médias, juntamente com vários ovos e embriões deste espécime. Como o nome indica, foi descoberto na Lourinhã, e é um dos esqueletos mais completos que temos em Portugal!<p>"
+
+const content4 = "<figure id = 'fig4'><img src='/images/torvosaurus.png' alt='Torvosaurus'width='440' height='288'>\
+<figcaption>Dentes de um Torvosaurus gurneyi</figcaption></figure>\
+<h3>Torvosaurus gurneyi</h3> <h5>Classificação:&nbspTerópode<br>Período:&nbspJurássico Superior</h5>\
+<p>Foi descoberta a maxila (parte superior do maxilar) de uma nova espécie de dinossauro, o Torvosaurus gurneyi, que é considerado o maior predador terreste da europa. Os fósseis foram descobertos por um paleontólogo Holandês amador no concelho de Peniche e a espécie foi batizada pela dupla de paleontólogos Octávio Mateus e Christophe Hendrickx.<p>"
+
+const content5 = "<figure id = 'fig5-1'><img src='/images/lusotitan1.jpg' alt='Lusotitan1'width='365' height='274'>\
+<figcaption>Osso do Lusotitan - Museu Geológico de Lisboa</figcaption></figure>\
+<figure id = 'fig5-2'><img src='/images/lusotitan2.jpg' alt='Lusotitan2' width='365' height='274'>\
+<figcaption>Osso do Lusotitan - Museu Geológico de Lisboa</figcaption></figure>\
+<h3>Lusotitan atalaiensis</h3> <h5>Classificação:&nbspSaurópode<br>Período:&nbspJurássico Superior</h5>\
+<p>O Lusotitan atalaiensis é uma espécie de dinossauro herbívoro de grande porte, atingindo até mais de 20 metros de comprimento. O primeiro registo fóssil desta espécie vem da década de 1940, na zona da Atalaia, onde o geólogo Manuel de Matos descobriu grandes fósseis de um Saurópode. Apenas em 1957 é que foi descrita como Brachiosaurus atalaiensis, sendo depois renomeada em 2003 por Octávio Mateus e Miguel Telles Antunes, para o seu nome atual. Os seus grandes ossos fossilizados encontram-se no Museu Geológico de Lisboa.</p>"
+
+const content6 = "<h3>Dinheirosaurus Lourinhanensis</h3> <h5>Classificação:&nbspSaurópode<br>Período:&nbspJurássico Superior</h5>\
+<p>O Dinheirosaurus Lourinhanensis é o dinossauro mais comprido encontrado em Portugal, chegando a atingir 25 metros de comprimento, apesar do seu grande porte, possuíam cabeças relativamente pequenas, com apenas 40-50cm de comprimento. Os seus fósseis foram descobertos na década de 1980, na praia de Porto Dinheiro, no concelho da Lourinhã, daí o seu nome.<p>\
+<b>Facto Curioso: Para afugentar possíveis predadores, estes dinossauros utilizavam a sua cauda como chicote e esta chegava a ultrapassar a velocidade do som, causando um estrondo sónico!</b>"
+
+const content7 = "<figure id = 'fig7'><img src='/images/museudalourinha.jpg' alt='Zby'width='500' height='333'>\
+<figcaption>Fóssil de Zby Atlanticus, na fachada do Museu da Lourinhã</figcaption></figure>\
+<h3>Zby Atlanticus</h3> <h5>Classificação:&nbspSaurópode<br>Período:&nbspJurássico Superior</h5>\
+<p>Zby Atlanticus, nomeado em honra de Georges Zbyszewski, foi outro Saurópode descoberto na Formação Lourinhã. Foi descrito por Octávio Mateus, Philip D. Mannion e Paul Upchurch em 2014. Os seus fósseis encontram-se na fachada do Museu da Lourinhã.<p>"
+
+const content8 = "<figure id = 'fig8'><img src='/images/miragaia.jpg' alt='Miragaia'width='440' height='244'>\
+<figcaption>Fóssil de Miragaia Longicollum</figcaption></figure>\
+<h3>Miragaia Longicollum</h3> <h5>Classificação:&nbspOrnitísquio<br>Período:&nbspJurássico Superior</h5>\
+<p>Esta espécie de dinossauro da família Stegosauridae foi encontrada em Miragaia, e possui 17 vértebras cervicais, daí o seu nome Longicollum (pescoço grande). Foi descoberta na década de 1990 e foi descrita em 2009 por Octávio Mateus. É o estegossauro mais completo descoberto na Europa e o seu crânio é o único conhecido de um estegossauro na Europa. Foram ainda encontradas pegadas deste dinossauro com impressão de pele. O seu esqueleto está em exposição no Dino Park, na Lourinhã.<p>"
+
+const content9 = "<figure id = 'fig9'><img src='/images/baryonyx.jpg' alt='Baryonyx'width='373' height='309'>\
+<figcaption>Fragmento de mandíbula e dentes de Baryonyx - Museu Geológico em Lisboa</figcaption></figure>\
+<h3>Baryonyx Walkeri</h3> <h5>Classificação:&nbspTerópode<br>Período:&nbspCretácico Inferior</h5>\
+<p>Os primeiros fósseis deste carnívoro bípede de média dimensão foram encontrados em 1983, em Inglaterra, por um colecionador de fósseis amador chamado William Walker. Em 1999 foram descobertos em Portugal alguns dentes e ossos fósseis deste dinossauro, na zona do Cabo Espichel. O seu nome significa Garras Pesadas e deve-se ao facto das suas garras terem cerca de 30 centímetros cada. Acredita-se que este seja o primeiro terópode piscívoro. Os fósseis deste dinossauro podem ser vistos no Museu da Lourinhã, e no Museu Geológico em Lisboa.<p>"
+
+const content10 = "<figure id = 'fig10'><img src='/images/dracopelta.jpg' alt='Dracopelta'width='388' height='293'>\
+<figcaption>Esqueleto parcial de Dracopelta - Museu Geológico em Lisboa</figcaption></figure>\
+<h3>Dracopelta zbyszewskii</h3> <h5>Classificação:&nbspOrnitísquio<br>Período:&nbspJurássico Superior</h5>\
+<p>Este dinossauro foi descoberto em 1980 por Peter Galton, a zona da descoberta é uma questão controversa pois não se sabe ao certo se foi em Ribamar, no concelho da Ericeira ou em Ribamar, no concelho da Lourinhã, mas acredita-se que o mais provável é ter ocorrido no concelho da Ericeira. Este dinossauro era de pequeno porte e tinha o seu dorso revestido de placas ósseas. É possível observar o seu esqueleto fossilizado no Museu Geológico de Lisboa e no Museu da Lourinhã.<p>"
+
+const content11 = "<figure id = 'fig11'><img src='/images/lusovenator.png' alt='Lusovenator'width='368' height='304'>\
+<figcaption>Elementos esqueléticos de Lusovenator santosi</figcaption></figure>\
+<h3>Lusovenator Santosi</h3> <h5>Classificação:&nbspTerópode<br>Período:&nbspJurássico Superior</h5>\
+<p>A mais recente descoberta da Paleontologia portuguesa foi o Lusovenator Santosi, um terópode de porte médio. Este dinossauro foi identificado a partir de restos recolhidos nas duas últimas décadas, nas jazidas das praias de Valmitão, na Lourinhã, e de Cambelas, em Torres Vedras. “Lusovenator” significa Caçador de Portugal, e “Santosi” é em homenagem a José Joaquim dos Santos, que descobriu fósseis deste dinossauro. Foi descrito em 2020 por Elisabete Malafaia.<p>"
+
+const content12 = "<figure id = 'fig12'><img src='/images/crocodilo.jpg' alt='Portugalosuchus'width='286' height='320'>\
+<figcaption>Fóssil de Portugalosuchus azenhae, Direitos Reservados</figcaption></figure>\
+<h3>Portugalosuchus azenhae</h3><h5>Classificação:&nbspCrocodylia<br>Período:&nbspCretácico Superior</h5>\
+<p>Em 2003, paleontólogos portugueses descobriram uma nova espécie de crocodilo fóssil com 95 milhões de anos. Foi encontrado em Tentúgal e, em 2018, a espécie foi batizada de Portugalosuchus azenhae, para homenagear Matilde Azenha, que foi quem descobriu o crânio fossilizado deste crocodilo. Como afirma Octávio Mateus, “Este fóssil é único na sua anatomia e é a peça-chave para compreender as fases iniciais da evolução dos crocodilos, por ser o mais antigo do grupo a que chamamos Crocodylia, os verdadeiros crocodilos”.<p>"
+
+const content13 = "<figure id = 'fig13'><img src='/images/trilobite.jpg' alt='Trilobite'width='440' height='300'>\
+<figcaption>Fóssil de Trilobite - Museu das Trilobites de Arouca</figcaption></figure>\
+<h3>Trilobites Gigantes</h3> <h5>Classificação:&nbspArtrópodes<br>Período:&nbspOrdoviciano</h5>\
+<p>Foi descoberto, na região de Arouca, o maior e mais completo conjunto fóssil de trilobites do mundo. Até à descoberta, apenas eram conhecidas trilobites com, no máximo,  10 centímetros de comprimento, mas algumas das trilobites encontradas em Arouca ultrapassavam os 30 centímetros de comprimento! O enorme tamanho destas trilobites leva a crer que estas viveram no período Ordovícico. É possível observar várias destas espécies de trilobites no Museu das Trilobites em Canelas. <p>"
+
+const content14 = "<figure id = 'fig14'><img src='/images/metoposaurus.jpg' alt='Metoposaurus'width='405' height='270'>\
+<figcaption>Fóssil de Metoposaurus algarvensis, Direitos Reservados</figcaption></figure>\
+<h3>Metoposaurus algarvensis</h3> <h5>Período:&nbspTriásico</h5>\
+<p>O Metoposaurus algarvensis é uma espécie de salamandra e era um dos maiores predadores há cerca de 200 milhões de anos atrás. Foi descoberto, na zona de Loulé, Algarve, por uma equipa de paleontólogos. Era contemporâneo das primeiras espécies de dinossauros e segundo Steve Brusatte, um dos cientistas do estudo, “Era tão comprido como um pequeno carro e tinha centenas de dentes afiados e uma grande cabeça chata. Este era o tipo de predador feroz que os primeiros dinossauros tinham que enfrentar”. Esta descoberta veio a demonstrar que a distribuição geográfica deste ser-vivo era maior do que se achava.<p>"
+
+const content15 = "<figure id = 'fig15'><img src='/images/Plesiopharos.jpg' alt='Plesiopharos'width='680' height='362'>\
+<figcaption>Fóssil de Allosaurus europaeus</figcaption></figure>\
+<h3>Plesiopharos moelensis</h3> <h5>Período:&nbspJurássico Inferior</h5>\
+<p>Um grupo de paleontólogos descobriu fósseis do Plesiossauro mais antigo e mais completo da Península Ibérica. A nova espécie, designada de Plesiopharos moelensis, foi encontrada na praia de São Pedro de Moel, na Marinha Grande, daí o seu nome “moelensis”. Data ao início do Período Jurássico, há cerca de 195 milhões de anos. Os  fósseis encontrados constituem parte das barbatanas do lado direito do animal, tórax e pescoço.<p>"
+
+function changeText(number) {
+  var targetDiv = document.getElementById("target");
+
+  switch(number){
+    case "1":
+      targetDiv.innerHTML = content1;
+      break;
+    case "2":
+      targetDiv.innerHTML = content2;
+      break;
+    case "3":
+      targetDiv.innerHTML = content3;
+      break;
+    case "4":
+      targetDiv.innerHTML = content4;
+      break;
+    case "5":
+      targetDiv.innerHTML = content5;
+      break;
+    case "6":
+      targetDiv.innerHTML = content6;
+      break;
+    case "7":
+      targetDiv.innerHTML = content7;
+      break;
+    case "8":
+      targetDiv.innerHTML = content8;
+      break;
+    case "9":
+      targetDiv.innerHTML = content9;
+      break;
+    case "10":
+      targetDiv.innerHTML = content10;
+      break;
+    case "11":
+      targetDiv.innerHTML = content11;
+      break;
+    case "12":
+      targetDiv.innerHTML = content12;
+      break;
+    case "13":
+      targetDiv.innerHTML = content13;
+      break;
+    case "14":
+      targetDiv.innerHTML = content14;
+      break;
+    case "15":
+      targetDiv.innerHTML = content15;
+      break;
+  }
+}
+
+(function() {
+  document.getElementById("b1").addEventListener("click", function() { changeText("1"); });
+  document.getElementById("b2").addEventListener("click", function() { changeText("2"); });
+  document.getElementById("b3").addEventListener("click", function() { changeText("3"); });
+  document.getElementById("b4").addEventListener("click", function() { changeText("4"); });
+  document.getElementById("b5").addEventListener("click", function() { changeText("5"); });
+  document.getElementById("b6").addEventListener("click", function() { changeText("6"); });
+  document.getElementById("b7").addEventListener("click", function() { changeText("7"); });
+  document.getElementById("b8").addEventListener("click", function() { changeText("8"); });
+  document.getElementById("b9").addEventListener("click", function() { changeText("9"); });
+  document.getElementById("b10").addEventListener("click", function() { changeText("10"); });
+  document.getElementById("b11").addEventListener("click", function() { changeText("11"); });
+  document.getElementById("b12").addEventListener("click", function() { changeText("12"); });
+  document.getElementById("b13").addEventListener("click", function() { changeText("13"); });
+  document.getElementById("b14").addEventListener("click", function() { changeText("14"); });
+  document.getElementById("b15").addEventListener("click", function() { changeText("15"); });
+})();
